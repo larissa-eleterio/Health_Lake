7d57894c537031a1b4d6eafcbe43554ab4a03b00
larissa-eleterio <larissa.eleterio@gmail.com>
2020-10-29 09:56:26 -0300

    Script de extração

---
+++ Extracao.py
@@ -0,0 +1,24 @@
+"""################### Importando os Pacotes ######################"""
+import pandas as pd
+import numpy as np
+import requests
+import json
+from statsmodels.tsa.seasonal import seasonal_decompose
+import matplotlib.pyplot as plt
+import seaborn as sns
+from datetime import datetime as dt
+import math
+from scipy.optimize import curve_fit
+sns.set_style()
+
+
+
+"""###################  Fonte dos dados ################### """
+
+url = "https://api.covid19api.com/total/country/brazil"
+
+payload = {}
+headers= {} 
+response = requests.request("GET", url, headers=headers, data = payload)
+
+#df = pd.json_normalize(response.json())
